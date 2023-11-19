45de37eed2183331bc64beda978c20af116298af
KasanaKartik <kartikkasanaji@gmail.com>
2023-11-19 14:01:13 +0530

    Finished Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+// function toggleMenu(){
+//     const menu = document.querySelector(".Menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
